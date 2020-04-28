ab8fa1aa965b23618da316424dc48ccc460a8ce6
Ankit Garg <technoera6345@gmail.com>
2020-04-28 20:16:00 +0530

    Carousel frontend done

---
+++ Interact.js
@@ -0,0 +1,12 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n){
+    showSlides(slideIndex=n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var thumbs = document
+}
